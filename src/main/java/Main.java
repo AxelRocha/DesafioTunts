ab0092a802b6eecfba829d92f6b3354270d16d91
Axel <oaoexoufpr@gmail.com>
2021-01-18 11:40:21 -0400

    testing reading Nome and Faltas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+import static Service.SheetService.getService;
+
 public class Main {
-    public static void main(String... args) {
-        System.out.println("Hello world");
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final String spreadsheetId = "1X-SDksJyPlQBXGpqKCSlB-1jORzInWZ6wJZUHYLIb8c";
+        final String range = "test_sheet!B4:C";
+
+        /*Create Service*/
+        Sheets service = getService();
+
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            System.out.println("Nome, Faltas");
+            for (List row : values) {
+                // Print columns A and E, which correspond to indices 0 and 4.
+                System.out.printf("%s, %s\n", row.get(0), row.get(1));
+            }
+        }
     }
 }
