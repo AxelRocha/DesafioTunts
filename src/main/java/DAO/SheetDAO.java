fdec0973c268830a0691276f52990c504bc023c9
Axel <oaoexoufpr@gmail.com>
2021-01-18 19:56:21 -0400

    adding code documentation

--- src/main/java/DAO/SheetDAO.java
+++ src/main/java/DAO/SheetDAO.java
@@ -19,7 +19,13 @@ public class SheetDAO {
         this.sheetName = sheetName + "!";
     }
 
-    private ValueRange getSheetValues(String range) throws IOException {
+    /**
+     * Gets the Data from the Sheet.
+     * @param range Range of data to get from the Sheet in A1 notation
+     * @return The requested range of data
+     * @throws IOException If the service can not execute
+     */
+    private ValueRange getSheetData(String range) throws IOException {
         lastResult = service.spreadsheets().values()
                 .get(spreadsheetId, range)
                 .setValueRenderOption("FORMATTED_VALUE")
@@ -28,16 +34,28 @@ public class SheetDAO {
         return lastResult;
     }
 
+    /**
+     * Gets the values from the data retrieve from the range param.
+     * @param range Range of data to get from the Sheet in A1 notation
+     * @return The data of the requested range
+     * @throws IOException If an error occurs getting the values
+     */
     public List<List<Object>> getValuesInRange(String range) throws IOException {
         if (lastRange.equals(range)) {
             return lastResult.getValues();
         }
 
         lastRange = range;
-        return getSheetValues(buildRange(sheetName, range))
+        return getSheetData(buildRange(sheetName, range))
                 .getValues();
     }
 
+    /**
+     * Set the values on the Sheet in the especified range param
+     * @param values values to be written on the Sheet
+     * @param range Range of data to where to be written on the Sheet in A1 notation
+     * @throws IOException If an error occurs setting the values
+     */
     public void setValuesInRange(List<List<Object>> values, String range) throws IOException {
         ValueRange body = new ValueRange()
                 .setValues(values);
